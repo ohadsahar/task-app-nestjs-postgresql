43bb62627ed128332c54adc73f82cae360f5aedd
Ohad <osahar5@gmail.com>
2019-08-13 14:16:32 +0300

    updated gitignore

--- task-backend/src/app.module.ts
+++ task-backend/src/app.module.ts
@@ -4,7 +4,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskModule } from './task/module/task.module';
-
 @Module({
   imports: [TaskModule, TypeOrmModule.forRoot({
     type: 'postgres',
